e700ce66e547df02a4d1650a69ab06338b30c043
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-08-15 21:03:25 -0700

    Fix styling and hovering

--- portfolio/src/components/ProjectPortfolio.tsx
+++ portfolio/src/components/ProjectPortfolio.tsx
@@ -1,28 +1,32 @@
 import { Box, Heading, Image, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { projectPages } from "../utils/data/constants";
 import { ProjectPage } from "../utils/types";
 
 const ProjectCard = ({ project }: { project: ProjectPage }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   let { name, cardImage, description } = project;
   return (
     <Box
       maxW="sm"
       h="initial"
       backgroundColor="gray.100"
-      borderRadius="2xl"
-      overflow="hidden"
+      borderBottomRadius="xl"
       boxSizing="border-box"
       transition="all 0.2s ease-in-out"
-      _hover={{
-        transform: "translateY(-30px)",
-      }}
-      boxShadow="lg"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      boxShadow={isHovered ? "2xl" : "lg"}
     >
-      <Image
-        borderTopRadius="2xl"
-        src={process.env.PUBLIC_URL + "images/" + cardImage}
-      />
+      <Box overflow="hidden">
+        <Image
+          src={process.env.PUBLIC_URL + "images/" + cardImage}
+          transform={isHovered ? "scale(1.2)" : "scale(1)"}
+          transition="all 0.2s ease-in-out"
+        />
+      </Box>
       <Box p={5}>
         <Heading size="md">{name}</Heading>
         <Text size="sm">{description}</Text>
