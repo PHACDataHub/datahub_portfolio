460bfb8ab17cc038b5189bf8f0952ea2c36ec19b
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-11-12 20:29:32 -0800

    fix sizing

--- src/components/organisms/ProjectPortfolio.tsx
+++ src/components/organisms/ProjectPortfolio.tsx
@@ -1,18 +1,20 @@
 import { Box, Heading } from '@chakra-ui/react';
 import { projectPageList } from '../../data/projectPages';
+import { useSmallScreen } from '../../utils/hooks';
 import { ProjectCard } from '../molecules/ProjectCard';
 
 export function ProjectPortfolio(): JSX.Element {
+  const isSmallScreen = useSmallScreen();
   return (
     <Box
       px={5}
-      py={10}
+      py={5}
       display="flex"
       flexDir="column"
       alignItems="center"
       w="100%"
     >
-      <Heading>Projects</Heading>
+      <Heading fontSize={isSmallScreen ? 20 : 24}>Projects</Heading>
       <Box
         py={8}
         display="flex"
@@ -21,7 +23,7 @@ export function ProjectPortfolio(): JSX.Element {
         // alignItems="flex-start"
         flexWrap="wrap"
         gap={8}
-        maxW="90%"
+        w={isSmallScreen ? '98%' : '80%'}
       >
         {projectPageList.map((project) => (
           <ProjectCard project={project} key={project.id} />
